16deb88e1175755bc1e3548a9a0a5c5309d9a5d5
GOLDG <goldgm@outlook.com>
2017-05-27 23:10:41 +0800

    新增功能代码

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var taskList = require('../controllers/TaskListController');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { listInfo: taskList.taskListInfoContr() });
 });
 
 module.exports = router;
